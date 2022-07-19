87e8ed0782fb0b6b0d24b4c7fe9d5f000af8e7eb
Kyle <ikyle53@icloud.com>
2022-07-19 12:48:21 -0700

    Second test passed

---
+++ modules/series.py
@@ -0,0 +1,18 @@
+def factorial(number):
+    if number <= 1:
+        return 1
+
+    if number > 1:
+        return number * (factorial(number - 1))
+
+# def fizz_buzz():
+    # pass
+# @pytest.mark.skip("pending")
+
+# pip install pytest-watch
+# ptw-c
+
+    # if __name__ == "__main__":
+        # x = input("Enter number > 1")
+        # result = factorial(int(x))
+        # print(f"factorial of {x} is {result}")
---
+++ test_series.py
@@ -0,0 +1,13 @@
+from modules.series import factorial
+
+
+def test_factorial_1():
+    actual = factorial(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_factorial_2():
+    actual = factorial(2)
+    expected = 2
+    assert actual == expected
